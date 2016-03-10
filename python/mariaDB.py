fc06f08065b14d9f218bda6cd223af037b67baf8
junwoo.kim <junwooo.kim@samsung.com>
2016-03-10 16:38:49 +0900

    add mariaDB connect example

---
+++ python/mariaDB.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python
+
+import MySQLdb
+
+from MySQLdb.constants import ER
+
+class mariaDBops:
+	DB_ip = ""
+	DB_id = ""
+	DB_pwd = ""
+	DB = ""
+	table = ""
+	
+	sql = "INSERT INTO "+DB+"."+table+" (imgurl, linkurl, price) VALUES (%s,%s,%s)"
+	def __init__(self):
+		self.db = MySQLdb.connect(DB_ip, DB_id, DB_pwd, DB)
+		self.cursor = self.db.cursor()
+		self.cursor.execute("SELECT VERSION()")
+		data = self.cursor.fetchone()
+		print "Database version : %s " % data
+
+	def __del__(self):
+		self.db.close()
+		
+	def add_item(self, link_url, img_url, price):
+		try:
+			self.cursor.execute(sql, (img_url, link_url, price))
+			#TODO: commit every time?
+			self.db.commit()
+		except MySQLdb.Error as e:
+			try:
+				if e.args[0] is not ER.DUP_ENTRY:
+					print "DB error : "+str(e)
+					db.rollback()
+			except Exception as e:
+				print "Exception : "+str(e)
+		
+		except Exception as e:
+			print "System Exception : "+str(e)
+			db.rollback()
