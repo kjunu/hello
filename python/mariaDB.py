1dd665abc745d2012463e29892ac5feaf06a7131
junwoo.kim <junwooo.kim@samsung.com>
2016-03-10 18:01:08 +0900

    fix python mariadb connect example

--- python/mariaDB.py
+++ python/mariaDB.py
@@ -13,7 +13,7 @@ class mariaDBops:
 	
 	sql = "INSERT INTO "+DB+"."+table+" (imgurl, linkurl, price) VALUES (%s,%s,%s)"
 	def __init__(self):
-		self.db = MySQLdb.connect(DB_ip, DB_id, DB_pwd, DB)
+		self.db = MySQLdb.connect(self.DB_ip, self.DB_id, self.DB_pwd, self.DB)
 		self.cursor = self.db.cursor()
 		self.cursor.execute("SELECT VERSION()")
 		data = self.cursor.fetchone()
@@ -24,17 +24,17 @@ class mariaDBops:
 		
 	def add_item(self, link_url, img_url, price):
 		try:
-			self.cursor.execute(sql, (img_url, link_url, price))
+			self.cursor.execute(self.sql, (img_url, link_url, price))
 			#TODO: commit every time?
 			self.db.commit()
 		except MySQLdb.Error as e:
 			try:
 				if e.args[0] is not ER.DUP_ENTRY:
 					print "DB error : "+str(e)
-					db.rollback()
+					self.db.rollback()
 			except Exception as e:
 				print "Exception : "+str(e)
 		
 		except Exception as e:
 			print "System Exception : "+str(e)
-			db.rollback()
+			self.db.rollback()
