e2a6085b30242b59704f46d80c1b814b4a307925
jwkim <jwkim@suprema.co.kr>
2017-12-07 14:06:32 +0900

    add boost practice code for bind, asio

--- prac_code/bind.cpp
+++ prac_code/bind.cpp
@@ -3,6 +3,7 @@
 #include <boost/shared_ptr.hpp>
 #include <boost/asio.hpp>
 #include <boost/asio/steady_timer.hpp>
+#include <boost/function.hpp>
 #include <boost/thread/thread.hpp>
 #include <boost/date_time/posix_time/posix_time.hpp>
 
@@ -25,6 +26,7 @@ void callback(const boost::system::error_code& ec)
 	sleep(1);
 	//TRACE("good morning callback! after 1");
 }
+
 class testman
 	: public boost::enable_shared_from_this<testman>
 {
@@ -32,28 +34,38 @@ public:
 	std::string name;
 	boost::asio::io_service io;
 	boost::shared_ptr<boost::asio::io_service::work> work_ptr;
-	testman(){
+
+	testman()
+	{
 		testman("static jw");
 	}
-	testman(const std::string& n){
+	
+	testman(const std::string n)
+	{
 		name = n;
 		TRACE(name);
+		//bad
+		//func = boost::bind(&testman::bindfunc, this, _1, _2);
 	}
+
 	static void postcallback()
 	{
 		TRACE("postcallback()");
 		sleep(1);
 		TRACE("hello! after 1");
 	}
+
+	void bindfunc(std::string a, int b)
+	{
+		TRACE(" bind a=" + a + ",b="+  std::to_string(b));
+		sleep(1);
+	}
+
 	int test_asio()
 	{
 		TRACE("start");
 		work_ptr.reset(new boost::asio::io_service::work(io));
 		boost::thread thread1(boost::bind(&boost::asio::io_service::run, &io));
-		//boost::thread thread2(boost::bind(&boost::asio::io_service::run, &io));
-		
-		//for (int i = 0 ; i < 3000000; i ++ )
-		{
 		boost::shared_ptr<boost::asio::deadline_timer> t(new boost::asio::deadline_timer(io));//, boost::posix_time::seconds(2));
 		boost::asio::deadline_timer t2(io);//, boost::posix_time::seconds(2));
 		//boost::asio::steady_timer t(io);
@@ -66,29 +78,48 @@ public:
 		//TRACE("set timer 2");
 		t->async_wait(&callback);
 		//sleep(1);
-		t->cancel();
+		//t->cancel();
 		//t2.async_wait(&callback);
 		//boost::thread thread2(boost::bind(&boost::asio::io_service::run, &io));
 		//testman jw("jw");
 		//io.post(&testman::postcallback);
+		std::string a = "a";
+		int b = 0xb;
+		//io.post(boost::bind(&testman::bindfunc, this, boost::ref(a), b));
 		sleep(3);
-		//cnt = t->cancel();
-		//t.reset(new boost::asio::deadline_timer(io));
-		TRACE("main slept 4");
+		
+		//bad
+		func(a, b);
+		TRACE("main slept 4, a=" + a);
 		std::cout << "cancel cnt = " << cnt << std::endl;
-		//t->expires_from_now(boost::posix_time::seconds(1));
-		//t->async_wait(&callback);
-		}
 		work_ptr.reset();
 		thread1.join();
 		//thread2.join();
 		TRACE("test main end");
 		return 0;
 	}
+	void set_func(boost::function<void(std::string, int)> f)
+	{
+		func = f;
+	}
+	boost::function<void(std::string, int)> func;
 };
-
+class player
+{
+	public:
+		player(testman& man)
+		{
+			//man.func = boost::bind(&player::play, this, _1, _2);
+			man.set_func(boost::bind(&player::play, this, _1, _2));
+		}
+		void play(std::string name, int age)
+		{
+			TRACE(" name=" + name + ",age="+  std::to_string(age));
+		}
+};
 int main()
 {
 	testman jw;
+	player p(jw);
 	return jw.test_asio();
 }
